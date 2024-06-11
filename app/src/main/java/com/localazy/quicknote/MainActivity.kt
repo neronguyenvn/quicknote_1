ad666427d30f760b15cf2d090b54a759de3d0ebb
neronguyenvn <hungnd.vnse@gmail.com>
2024-06-11 15:34:47 +0700

    feat: implement foreground service to handle 2 commands

--- app/src/main/java/com/localazy/quicknote/MainActivity.kt
+++ app/src/main/java/com/localazy/quicknote/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.localazy.quicknote
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import com.localazy.quicknote.notes.NotesViewModel
@@ -15,6 +17,19 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val notificationPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { granted ->
+            if (granted) startFloatingService()
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            notificationPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+        } else {
+            startFloatingService()
+        }
+
         setContent {
             Column {
                 AddNote(getString(R.string.add_note)) {
